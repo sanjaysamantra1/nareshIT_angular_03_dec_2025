022f29bdbaeb73e6de26dc88b457969cd10a5232
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-12-30 08:55:31 +0530

    angular day 2

---
+++ angular/angular_project_1/src/app/components/demo1/demo1.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo1',
+  imports: [],
+  template: `
+    <p>
+      demo1 works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Demo1 {
+
+}
