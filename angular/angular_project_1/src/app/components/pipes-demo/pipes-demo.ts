37ecf5e37cf3e5e8fad7b3dfa966457aa0c427f2
Sanjay Samantra <sanjaysamantra1@gmail.com>
2026-01-09 08:46:54 +0530

    pipes

--- angular/angular_project_1/src/app/components/pipes-demo/pipes-demo.ts
+++ angular/angular_project_1/src/app/components/pipes-demo/pipes-demo.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RemainingPipe } from '../../custom-pipes/remaining-pipe';
 import { OrdinalPipe } from '../../custom-pipes/ordinal-pipe';
+import { MysortPipe } from '../../custom-pipes/mysort-pipe';
 
 @Component({
   selector: 'app-pipes-demo',
@@ -10,7 +11,8 @@ import { OrdinalPipe } from '../../custom-pipes/ordinal-pipe';
     CommonModule,
     FormsModule,
     RemainingPipe,
-    OrdinalPipe
+    OrdinalPipe,
+    MysortPipe
   ],
   templateUrl: './pipes-demo.html',
   styleUrl: './pipes-demo.css',
@@ -23,7 +25,8 @@ export class PipesDemo {
   arr = [10, 20, 30, 40, 50, 60, 70];
   msg: string = '';
   msg2: string = '';
-  num:number = 21;
+  num: number = 21;
+  numArr = [20, 50, 10, 40, 30]
 
   keepOriginalOrder = function () {
     console.log('keepOriginalOrder')
--- angular/angular_project_1/src/app/custom-pipes/ordinal-pipe.ts
+++ angular/angular_project_1/src/app/custom-pipes/ordinal-pipe.ts
@@ -2,11 +2,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'ordinal',
+  // pure: false // impure
 })
 export class OrdinalPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: number) {
+    console.log('ordinal pipe called...')
+    let rem = value % 10;
+
+    if (rem == 1) {
+      return value + 'st';
+    } else if (rem == 2) {
+      return value + 'nd';
+    } else if (rem == 3) {
+      return value + 'rd';
+    } else {
+      return value + 'th';
+    }
   }
 
 }
